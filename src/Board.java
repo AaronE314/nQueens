a0f8b04556f02b151b5ed5d74da673d41f450e70
Aaron Exley <aaronde314@gmail.com>
2016-07-12 00:41:05 -0400

    added comments

--- src/Board.java
+++ src/Board.java
@@ -5,7 +5,6 @@ public class Board {
     private static int board[][];
     private int numQueens;
     private static int size;
-    private static int numofcalls = 0;
 
     public Board(int n) {
         numQueens = 0;
@@ -19,24 +18,29 @@ public class Board {
     }
 
     public void start() {
+        //starts the solve with 0 queens
         solve(0);
     }
 
+    //solves the board
     public boolean solve(int numQueens) {
-        //numofcalls++;
-        //System.out.println("call: " + numofcalls);
+        //checks if the current number of queens is equal to the max
         if (numQueens == size) {
             this.printBoard();
             return true;
         } else {
             for (int i = 0; i < size; i++) {
                 for (int j = 0; j < size; j++) {
+                    //if its a valid move
                     if (validMove(i, j) == 0) {
+                        //place queen
                         this.placeQueen(i, j, 0);
                         numQueens++;
+                        //calles function again and checks if its true
                         if (solve(numQueens)) {
                             return true;
                         } else {
+                            //if its not it removes queen and trys again
                             this.placeQueen(i, j, 1);
                             numQueens--;
                         }
@@ -47,16 +51,19 @@ public class Board {
         return false;
     }
 
+    //check to see if a spot if valid
     public static int validMove(int x, int y) {
+        //checks horizontal
         for (int i = 0; i < size; i++) {
             if (get(x, i) == 1) {
                 return -1;
             }
+            //checks vertical
             if (get(i, y) == 1) {
                 return -1;
             }
         }
-        //check diag
+        //check diagonal
         for (int i = 0; i < size; i++) {
             if (get(x - i, y - i) == 1) {
                 return -1;
@@ -74,6 +81,7 @@ public class Board {
         return 0;
     }
 
+    //0 is place queen, 1 is remove queen
     public int placeQueen(int x, int y, int type) {
         if (type == 0) {
             board[x][y] = 1;
@@ -86,6 +94,7 @@ public class Board {
         return -3;
     }
 
+    //gets value of tile
     public static int get(int x, int y) {
         if (x < 0 || y < 0 || x > size - 1 || y > size - 1) {
             return -1;
@@ -93,6 +102,7 @@ public class Board {
         return board[x][y];
     }
 
+    //prints board
     public void printBoard() {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 
+
+/* nQueens problem
+     find out how to place n number of queens in an nxn board so that they can't attack each other (can't share same vertical, horizontal, or diagonal) */
 public class Main {
     public static void main(String[] args) {
         Scanner sin = new Scanner(System.in);
